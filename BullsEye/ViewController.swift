60ede0ec88b93602d3ce90d2604d422ca26e70de
Luisk Hernandez <luis.macias@koombea.com>
2018-07-13 17:24:04 -0500

    Update lables and conect Outlet to values

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,22 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 50
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    var targetValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func sliderMoved (_ slider: UISlider) {
+        print("The value of slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels () {
+        targetLabel.text = String(targetValue)
+    }
+    
     @IBAction func showAlert() {
-        let alert  = UIAlertController(title: "Hello", message: "This is my first app", preferredStyle: .alert)
+        let message = "The value of the slider is: \(currentValue)" +
+         "\nThe target value is \(targetValue)"
+        let alert  = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome" , style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
 }
